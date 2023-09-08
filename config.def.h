34d12d9d343ac1dcd7fa344b754e21481cf578a5
alyssa <alyssa>
2023-09-08 00:31:58 +0000

    add pywal colors

--- config.def.h
+++ config.def.h
@@ -108,41 +108,7 @@ unsigned int tabspaces = 8;
 /* bg opacity */
 float alpha = 0.8;
 
-static const char *colorname[] = {
-	/* 8 normal colors */
-	"#45475A",
-	"#F38BA8",
-	"#A6E3A1",
-	"#F9E2AF",
-	"#89B4FA",
-	"#F5C2E7",
-	"#94E2D5",
-	"#BAC2DE",
-
-	/* 8 bright colors */
-	"#585B70",
-	"#F38BA8",
-	"#A6E3A1",
-	"#F9E2AF",
-	"#89B4FA",
-	"#F5C2E7",
-	"#94E2D5",
-	"#A6ADC8",
-
-[256] = "#FFFFFF", /* default foreground colour */
-[257] = "#000000", /* default background colour */
-[258] = "#F5E0DC", /*575268*/
-
-};
-
-
-/*
- * foreground, background, cursor, reverse cursor
- */
-unsigned int defaultfg = 256;
-unsigned int defaultbg = 257;
-unsigned int defaultcs = 258;
-static unsigned int defaultrcs = 258;
+#include "/home/alyssa/.cache/wal/colors-wal-st.h"
 
 /*
  * Default shape of cursor
@@ -151,7 +117,7 @@ static unsigned int defaultrcs = 258;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 6;
 
 /*
  * Default columns and rows numbers
