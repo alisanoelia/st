9d02b4d2281ae8684c744e2a08ba21272500c077
alyssa <alyssa>
2024-06-07 15:28:15 -0500

    update: new color scheme kolore

--- kolore.h
+++ kolore.h
@@ -1,29 +1,29 @@
 const char *colorname[] = {
 
     /* Base colors */
-    [0] = "#0d1617", // Black
+    [0] = "#00171a", // Black
     [1] = "#f85552", // red
-    [2] = "#1f6f4f", // green
+    [2] = "#36a77c", // green
     [3] = "#dfa000", // yellow
     [4] = "#3a94c5", // blue
-    [5] = "#b71b8f", // magenta
-    [6] = "#35a77c", // cyan
+    [5] = "#d33682", // magenta
+    [6] = "#33b5aa", // cyan
     [7] = "#d3cccc", // white
 
     /* Colores brillantes */
 
     [8] = "#d3cccc",
     [9] = "#f85552",
-    [10] = "#1f6f4f",
+    [10] = "#36a77c",
     [11] = "#dfa000",
     [12] = "#3a94c5",
-    [13] = "#b71b8f",
-    [14] = "#1fbfbf",
-    [15] = "#f1f1f1",
+    [13] = "#d33682",
+    [14] = "#33b5aa",
+    [15] = "#f2f2f2",
 
-    [256] = "#0d1617",
-    [257] = "#d3cccc",
-    [258] = "#d3cccc",
+    [256] = "#00171a",
+    [257] = "#f2f2f2",
+    [258] = "#f2f2f2",
 };
 
 /* Default colors (colorname index)
