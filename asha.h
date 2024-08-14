0674e42adfaeccf6cb967a1b5b4716ee81e56101
alyssa <alyssa>
2024-08-14 18:13:45 +0000

    update: to asha colorscheme

--- asha.h
+++ asha.h
@@ -2,28 +2,30 @@
 static const char *colorname[] = {
 
   /* 8 normal colors */
-  [0] = "#2e2e30", /* black   */
+  [0] = "#373b3f", /* black   */
   [1] = "#91615e", /* red     */
   [2] = "#58784e", /* green   */
   [3] = "#a18a5d", /* yellow  */
   [4] = "#607f94", /* blue    */
   [5] = "#997459", /* magenta */
   [6] = "#5f8787", /* cyan    */
-  [7] = "#cddbdb", /* white   */
+  [7] = "#9ba9aa", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#2a2e30", /* black   */
+  [8]  = "#373b3f", /* black   */
   [9]  = "#91615e", /* red     */
   [10] = "#58784e", /* green   */
   [11] = "#a18a5d", /* yellow  */
   [12] = "#607f94", /* blue    */
   [13] = "#997459", /* magenta */
   [14] = "#5f8787", /* cyan    */
-  [15] = "#cddbdb", /* white   */
+  [15] = "#9ba9aa", /* white   */
 
   /* special colors */
-  [256] = "#051215", /* background */
-  [257] = "#cddbdb", /* foreground */
+  /* [256] = "#051215",  */
+  /* [256] = "#07191d",  */
+  [256] = "#081d21", 
+  [257] = "#dcddde", /* foreground */
 };
 
 /*
