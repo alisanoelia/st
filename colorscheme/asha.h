7aa61e25a26140538750c79472ebe3e3a6a78a00
asha <asha>
2024-12-15 00:23:15 -0500

    [update]: remove and add new color schemes

--- colorscheme/asha.h
+++ colorscheme/asha.h
@@ -2,29 +2,30 @@
 static const char *colorname[] = {
 
     /* 8 normal colors */
-    [0] = "#1c2b30", /* black   */
+    [0] = "#232a2d", /* black   */
     [1] = "#915a5a", /* red     */
     [2] = "#5c7a53", /* green   */
     [3] = "#a18a5d", /* yellow  */
     [4] = "#607f94", /* blue    */
     [5] = "#997459", /* magenta */
     [6] = "#5f8787", /* cyan    */
-    [7] = "#f4ebd7", /* white   */
+    [7] = "#b3b9b8", /* white   */
 
     /* 8 bright colors */
-    [8] = "#959496",  /* black   */
+    [8] = "#2a3437",  /* black   */
     [9] = "#915a5a",  /* red     */
     [10] = "#5c7a53", /* green   */
     [11] = "#a18a5d", /* yellow  */
     [12] = "#607f94", /* blue    */
     [13] = "#997459", /* magenta */
     [14] = "#5f8787", /* cyan    */
-    [15] = "#f4ebd7", /* white   */
+    [15] = "#bdc3c2", /* white   */
 
     /* special colors */
-    [256] = "#071113",
+    /* [256] = "#090F11", */
+    [256] = "#101516",
     /* [257] = "#f8e7c2",  */
-    [257] = "#f4ebd7",
+    [257] = "#c5c8c6",
 };
 
 /*
---
+++ colorscheme/pome.h
@@ -0,0 +1,33 @@
+// Creando pome colorscheme
+static const char *colorname[] = {
+
+    [0] = "#191b1c", /* black   */
+    [1] = "#ea6962", /* red     */
+    [2] = "#a9b665", /* green   */
+    [3] = "#d8a657", /* yellow  */
+    [4] = "#7daea3", /* blue    */
+    [5] = "#e78a4e", /* magenta */
+    [6] = "#89b482", /* cyan    */
+    [7] = "#ebdbb2", /* white   */
+
+    [8] = "#2a3437",  /* black   */
+    [9] = "#ea6962", /* red     */
+    [10] = "#a9b665", /* green   */
+    [11] = "#d8a657", /* yellow  */
+    [12] = "#7daea3", /* blue    */
+    [13] = "#e78a4e", /* magenta */
+    [14] = "#89b482", /* cyan    */
+    [15] = "#ebdbb2", /* white   */
+
+    [256] = "#1d2021",
+    [257] = "#ebdbb2",
+};
+
+/*
+ * Default colors (colorname index)
+ * foreground, background, cursor
+ */
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
+unsigned int defaultrcs = 258;
